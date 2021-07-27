b2f5ffddff533e3a8237c62492f2ac81494061d4
Jordan Utrera <axeljordanomu@gmail.com>
2021-07-27 00:20:14 -0500

    Añadicion de nuevo cout

--- hola.cpp
+++ hola.cpp
@@ -8,5 +8,6 @@ int main(){
 	cout<<"Hola mundo"<<endl;
 	cout<< "esto es una nueva linea de prueba"<<endl;
 	cout<<"esta es la modificacion para hacer una fusion de las lineas"<< endl;
+	cout<<"Esto es un nuevo cambio para proobar el reemplazar y otras cosillas"<<endl;
 	return 0;
 }
