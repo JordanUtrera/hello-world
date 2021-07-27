7ff257953b3fa17c26af7e6af432ffb29d4dca0c
JordanUtrera <78768750+JordanUtrera@users.noreply.github.com>
2021-07-26 22:59:35 -0500

    no se esto es un cambio loco

--- hola.cpp
+++ hola.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-	//no se esto es un comentario en el cual veo las cosas que se cambiaron o no.
+	//no se esto es un comentario en el cual veo las cosas que se
 	cout<<"Hola mundo"<<endl;
 	return 0;
 }
