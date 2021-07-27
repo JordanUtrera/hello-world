833601416066e0129162e9bb3f91758255880103
Jordan Utrera <axeljordanomu@gmail.com>
2021-07-26 23:52:14 -0500

    modificacion de nuevos couts

--- hola.cpp
+++ hola.cpp
@@ -7,5 +7,6 @@ int main(){
 	//no se algo nuevo por aqui
 	cout<<"Hola mundo"<<endl;
 	cout<< "esto es una nueva linea de prueba"<<endl;
+	cout<<"esta es la modificacion para hacer una fusion de las lineas"<< endl;
 	return 0;
 }
